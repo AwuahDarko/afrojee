43307d9f3dc525e34514c45914c1bd4046f9dbac
Maxwell <nanayawfixing@gmail.com>
2025-05-14 08:17:23 +0800

    good

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,7 @@ module.exports = {
     ],
     theme: {
         extend: {
-            colors: {
-                gold: "#C8A655",
-                cream: "#F8F6F1",
-                rosegold: "#E0C3B6",
-                footergold: "#A67C24",
-                theme: "var(--color-theme)",
-                "theme-light": "var(--color-theme-light)",
-            },
+            colors: {},
         },
     },
     plugins: [],
