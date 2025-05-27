7cf296bd1e96840f6dff861273f57a64593b3711
Maxwell <nanayawfixing@gmail.com>
2025-05-27 08:41:54 +0800

    cart done

--- resources/views/frontend/partials/cartItems.blade.php
+++ resources/views/frontend/partials/cartItems.blade.php
@@ -3,9 +3,8 @@
 
     <div class="grid md:grid-cols-3 gap-8">
         <div id="cart-items-container" class="md:col-span-2 bg-white rounded-xl p-4 shadow-sm">
-            <div class="cart-item flex items-center p-4" data-price="20.00">
-                <img src="/images/p3.png" alt="Product Image"
-                    class="w-20 h-20 rounded-lg mr-4">
+            <div class="cart-item flex items-center p-8" data-price="20.00">
+                <img src="/images/p3.png" alt="Product Image" class="w-25 h-25 rounded-lg mr-4">
                 <div class="flex-grow">
                     <h1 class="font-semibold pb-10 text-lg">Always moisturized Body Butter</h1>
                     <div class="flex items-center text-gray-600 align-middle">
@@ -18,10 +17,13 @@
                             readonly>
                         <button
                             class="quantity-plus px-2 py-1 border border-gray-300 rounded-full hover:bg-gray-100 transition duration-200 mr-auto">+</button>
-                        <button class="delete-item ml-4 text-gray-500 hover:text-red-600 transition duration-200">
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                        <button
+                            class="delete-item px-2 py-1 border border-gray-300 rounded-full hover:bg-gray-100 transition duration-200">
+                            <svg width="17" height="21" viewBox="0 0 17 21" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path
-                                    d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+                                    d="M1.702 5.43h14m-9 3v8m4-8v8m-4-15h4a1 1 0 0 1 1 1v3h-6v-3a1 1 0 0 1 1-1m-4 4h12v13a1 1 0 0 1-1 1h-10a1 1 0 0 1-1-1z"
+                                    stroke="#C63636" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                         </button>
                     </div>
@@ -30,9 +32,8 @@
             <div class="border-b border-gray-300 mx-4"></div>
 
 
-            <div class="cart-item flex items-center p-4" data-price="20.00">
-                <img src="/images/p3.png" alt="Product Image"
-                    class="w-20 h-20 rounded-lg mr-4">
+            <div class="cart-item flex items-center p-8" data-price="20.00">
+                <img src="/images/p3.png" alt="Product Image" class="w-25 h-25 rounded-lg mr-4">
                 <div class="flex-grow">
                     <h1 class="font-semibold pb-10 text-lg">Always moisturized Body Butter</h1>
                     <div class="flex items-center text-gray-600 align-middle">
@@ -45,10 +46,13 @@
                             readonly>
                         <button
                             class="quantity-plus px-2 py-1 border border-gray-300 rounded-full hover:bg-gray-100 transition duration-200 mr-auto">+</button>
-                        <button class="delete-item ml-4 text-gray-500 hover:text-red-600 transition duration-200">
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                        <button
+                            class="delete-item px-2 py-1 border border-gray-300 rounded-full hover:bg-gray-100 transition duration-200">
+                            <svg width="17" height="21" viewBox="0 0 17 21" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path
-                                    d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+                                    d="M1.702 5.43h14m-9 3v8m4-8v8m-4-15h4a1 1 0 0 1 1 1v3h-6v-3a1 1 0 0 1 1-1m-4 4h12v13a1 1 0 0 1-1 1h-10a1 1 0 0 1-1-1z"
+                                    stroke="#C63636" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                         </button>
                     </div>
@@ -56,9 +60,8 @@
             </div>
             <div class="border-b border-gray-300 mx-4"></div>
 
-            <div class="cart-item flex items-center p-4" data-price="20.00">
-                <img src="/images/p3.png" alt="Product Image"
-                    class="w-20 h-20 rounded-lg mr-4">
+            <div class="cart-item flex items-center p-8" data-price="20.00">
+                <img src="/images/p3.png" alt="Product Image" class="w-25 h-25 rounded-lg mr-4">
                 <div class="flex-grow">
                     <h1 class="font-semibold pb-10 text-lg">Always moisturized Body Butter</h1>
                     <div class="flex items-center text-gray-600 align-middle">
@@ -71,10 +74,13 @@
                             readonly>
                         <button
                             class="quantity-plus px-2 py-1 border border-gray-300 rounded-full hover:bg-gray-100 transition duration-200 mr-auto">+</button>
-                        <button class="delete-item ml-4 text-gray-500 hover:text-red-600 transition duration-200">
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                        <button
+                            class="delete-item px-2 py-1 border border-gray-300 rounded-full hover:bg-gray-100 transition duration-200">
+                            <svg width="17" height="21" viewBox="0 0 17 21" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path
-                                    d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+                                    d="M1.702 5.43h14m-9 3v8m4-8v8m-4-15h4a1 1 0 0 1 1 1v3h-6v-3a1 1 0 0 1 1-1m-4 4h12v13a1 1 0 0 1-1 1h-10a1 1 0 0 1-1-1z"
+                                    stroke="#C63636" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                         </button>
                     </div>
